b2130bf0d7829e020f4288f410b54fa50211c317
Lu <lucebarcelona@gmail.com>
2024-02-01 20:01:42 +0100

    implement react typescript, organize project structure

---
+++ stubhub/src/App.tsx
@@ -0,0 +1,14 @@
+import { Footer } from "./containers/Footer";
+import { Header } from "./containers/Header";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      body
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
