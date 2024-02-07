e308bb71436fcda2e756765102f0b707451fe105
Lu <lucebarcelona@gmail.com>
2024-02-07 22:23:51 +0100

    add error handling when there's no tickets left

--- stubhub/src/containers/Event/index.tsx
+++ stubhub/src/containers/Event/index.tsx
@@ -8,8 +8,8 @@ import { Ticket } from "../../types/tickets.types";
 
 const Event = () => {
   const { eventId, eventName } = useParams<{ eventId: string; eventName: string }>();
-  const [eventData, setEventData] = useState<any[]>([]);
 
+  const [eventData, setEventData] = useState<any[]>([]);
   useEffect(() => {
     const fetchEventData = async () => {
       if (!eventId) {
@@ -29,31 +29,34 @@ const Event = () => {
   }, [eventId]);
 
   const formattedEventName = eventName ? eventName.replace(/-/g, " ") : "Default Event Name";
-
   return (
     <EventContainer>
       <h2>{formattedEventName}</h2>
-      <TicketList>
-        <table >
-          <thead>
-            <tr>
-              <th>Localidad</th>
-              <th>Disponibles</th>
-              <th>Precio por entrada</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            {eventData.map((ticket: Ticket, index: number) => (
-              <TicketItem
-                key={index}
-                quantity={ticket.quantity}
-                unit_price={ticket.unit_price}
-              />
-            ))}
-          </tbody>
-        </table>
-      </TicketList>
+      { eventData.length === 0 ? (
+        <p>No tickets left for this event.</p>
+      ) : (
+        <TicketList>
+          <table>
+            <thead>
+              <tr>
+                <th>Localidad</th>
+                <th>Disponibles</th>
+                <th>Precio por entrada</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {eventData.map((ticket: Ticket, index: number) => (
+                <TicketItem
+                  key={index}
+                  quantity={ticket.quantity}
+                  unit_price={ticket.unit_price}
+                />
+              ))}
+            </tbody>
+          </table>
+        </TicketList>
+      )}
     </EventContainer>
   );
 };
